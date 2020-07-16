7d7638c55a469a1dd4ba151dc390ccfb80dab8d1
Nahuel Oyhanarte <noyhanarte@gmail.com>
2020-07-16 03:59:13 -0300

    added img

--- app/index.js
+++ app/index.js
@@ -6,11 +6,16 @@ const express = require('express');
 const PORT = 8080;
 const HOST = '0.0.0.0';
 
+
 // App
 const app = express();
+app.use(express.static('assets'));
+
 app.get('/', (req, res) => {
-  res.send('Seguridad en Redes - UTN - FRBA');
+  res.send('<h1 style="text-aling:center"> Seguridad en Redes - UTN - FRBA </h1> <img src="portada.png" alt="Portada TP SR" />' );
 });
+
+
 
 app.listen(PORT, HOST);
 console.log(`Running on http://${HOST}:${PORT}`);
